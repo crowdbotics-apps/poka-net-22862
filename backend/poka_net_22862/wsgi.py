703eae016538fe57dd4ed2361f67f989d6ce5398
Crowdbotics <team@crowdbotics.com>
2020-11-21 16:06:04 +0000

    Update models

--- backend/poka_net_22862/wsgi.py
+++ backend/poka_net_22862/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poka_net_22862.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poka_net_22862.settings")
 
 application = get_wsgi_application()
